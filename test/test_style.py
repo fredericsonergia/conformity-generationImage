c4bb4914a5084d8bcc663dfd0f2393acca6a6afd
Matthieu Rossignon <matthieu.rossignon@student-cs.fr>
2021-02-10 17:24:44 +0100

    removes style before test not after

--- test/test_style.py
+++ test/test_style.py
@@ -10,15 +10,8 @@ if __name__ == "__main__":
     content = "./content"
     style = "./style"
     result = "./results"
-    print("starting test")
-    transfer_random_style_folder(content, style, result)
-    print("end test")
-    try:
-        assert len(os.listdir(result)) == 2
-        print("test passed")
-    except Exception as e:
-        print(f"test failed because of {e}")
 
+    # empty results folder
     for filename in os.listdir(result):
         file_path = os.path.join(result, filename)
         try:
@@ -28,3 +21,12 @@ if __name__ == "__main__":
                 shutil.rmtree(file_path)
         except Exception as e:
             print("Failed to delete %s. Reason: %s" % (file_path, e))
+
+    print("starting test")
+    transfer_random_style_folder(content, style, result)
+    print("end test")
+    try:
+        assert len(os.listdir(result)) == 2
+        print("test passed")
+    except Exception as e:
+        print(f"test failed because of {e}")
